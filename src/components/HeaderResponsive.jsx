28b4afc6894eccb070f7dc2eef983c7160c55f57
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-07-13 18:12:05 -0500

    Feat: menu responsive

--- src/components/HeaderResponsive.jsx
+++ src/components/HeaderResponsive.jsx
@@ -1,5 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const HeaderResponsive = () => {
   return (
@@ -34,18 +35,33 @@ const HeaderResponsive = () => {
             leaveTo="transform opacity-0 scale-95"
           >
             <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="px-1 py-1 ">
+              <div className="">
                 <Menu.Item>
                   {({ active }) => (
-                    <button
+                    <Link
+                      to="Soluciones"
                       className={`${
                         active
                           ? "bg-gradient-to-r from-orange-500 to-orange-700 text-white"
                           : "text-gray-900"
-                      } group flex w-full items-center rounded-md p-2 text-xl font-semibold justify-center`}
+                      } group flex w-full items-center rounded-md p-2 text-xl font-semibold justify-start gap-2`}
                     >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                        />
+                      </svg>
                       Soluciones
-                    </button>
+                    </Link>
                   )}
                 </Menu.Item>
               </div>
@@ -53,20 +69,112 @@ const HeaderResponsive = () => {
               <div>
                 <Menu.Item>
                   {({ active }) => (
-                    <button
+                    <Link
+                      to="Sistema"
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        active
+                          ? "bg-gradient-to-r from-orange-500 to-orange-700 text-white"
+                          : "text-gray-900"
+                      } group flex w-full items-center rounded-md p-2 text-xl font-semibold justify-start gap-2`}
                     >
-                      {" "}
-                      hola
-                    </button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+                        />
+                      </svg>
+                      Sistema
+                    </Link>
                   )}
                 </Menu.Item>
               </div>
 
-              <div className="px-1 py-1">
-                <Menu.Item>{({ active }) => <button>Delete</button>}</Menu.Item>
+              <div>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to="Precios"
+                      className={`${
+                        active
+                          ? "bg-gradient-to-r from-orange-500 to-orange-700 text-white"
+                          : "text-gray-900"
+                      } group flex w-full items-center rounded-md p-2 text-xl font-semibold justify-start gap-2`}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      Precios
+                    </Link>
+                  )}
+                </Menu.Item>
+              </div>
+
+              <div>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to="Soporte"
+                      className={`${
+                        active
+                          ? "bg-gradient-to-r from-orange-500 to-orange-700 text-white"
+                          : "text-gray-900"
+                      } group flex w-full items-center rounded-md p-2 text-xl font-semibold justify-start gap-2`}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                        />
+                      </svg>
+                      Soporte
+                    </Link>
+                  )}
+                </Menu.Item>
+              </div>
+
+              <div>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div className="p-3">
+                      <button
+                        className={`${
+                          active
+                            ? "bg-gradient-to-r from-orange-500 to-orange-800 text-black"
+                            : "bg-gradient-to-r from-orange-500 to-orange-700 text-white"
+                        } w-full items-center rounded-md p-2 text-lg font-bold justify-center uppercase shadow-xl`}
+                      >
+                        Prueba Gratuita
+                      </button>
+                    </div>
+                  )}
+                </Menu.Item>
               </div>
             </Menu.Items>
           </Transition>
